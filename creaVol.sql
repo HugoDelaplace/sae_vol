9f2f4776f7751aef3da82ad01c24b82a004f2af4
DELAPLACE Hugo <o22106071@info21-23.iut45.univ-orleans.fr>
2023-04-03 12:30:30 +0200

    3.2

--- creaVol.sql
+++ creaVol.sql
@@ -1,25 +1,15 @@
 drop table VOL;
 drop table COMPAGNIE;
 drop table AEROPORT;
-drop type Equipage;
-drop type indiceQualite;
-drop type Qualite;
-drop type Avionneur;
+drop table EQUIPAGE;
+drop type indice_qualite;
 
-create type Avionneur as object(
-    nomE varchar(50),
-    fonction varchar(50)
+CREATE or replace TYPE indice_qualite AS TABLE OF NUMBER;
+
+CREATE TABLE EQUIPAGE(
+    nomE VARCHAR(50),
+    fonction VARCHAR(50)
 );
-
-create type Equipage as table of Avionneur;
-
-create type indiceQualite as object(
-    nomIndice varchar(50),
-    indice int,
-    poids int
-);
-
-create type Qualite as table of indiceQualite;
 
 create table VOL(
     idVol int,
@@ -30,8 +20,8 @@ create table VOL(
     terminalArr varchar(10),
     terminalDep varchar(10),
     dateHeureArr date,
-    equipage Equipage,
-    qualite Qualite
+    equipage REF EQUIPAGE,
+    indices_qualite indice_qualite
 );
 
 create table COMPAGNIE(
@@ -54,9 +44,8 @@ alter table COMPAGNIE add constraint PK_COMPAGNIE primary key (idComp);
 alter table AEROPORT add constraint PK_AEROPORT primary key (codeAero);
 
 alter table VOL add constraint FK_VOL_COMPAGNIE foreign key (idComp) references COMPAGNIE(idComp);
-alter table VOL add constraint FK_VOL_AEROPORT foreign key (codeAeroDep) references AEROPORT(codeAero);
-alter table VOL add constraint FK_VOL_AEROPORT2 foreign key (codeAeroArr) references AEROPORT(codeAero);
-alter
+alter table VOL add constraint FK_VOL_AEROPORT_DEPART foreign key (codeAeroDep) references AEROPORT(codeAero);
+alter table VOL add constraint FK_VOL_AEROPORT_ARRIVEE foreign key (codeAeroArr) references AEROPORT(codeAero);
 
 alter SESSION set NLS_DATE_FORMAT = 'yyyy/dd/mm HH24:MI:SS'
 
@@ -68,12 +57,12 @@ insert into AEROPORT values (1,'Charles de Gaulle','Paris','France');
 insert into AEROPORT values (3,'Aéroport de Montréal','Montréal','Canada');
 insert into AEROPORT values (4,'Aéroport de Québec','Québec','Canada');
 insert into AEROPORT values (5,'Aéroport d’Alger','Alger','Algérie');
-insert into AEROPORT values (6,'Aéroport de Tamanrasset','Tamanrasset','Algérie');
+insert into AEROPORT values (6,'Aéroport de Tamanrasset','Tamanrasset','Algérie'); 
 
-insert into VOL values (1,1,to_date('2019-01-01 10:00:00', 'yyyy/mm/dd hh24:mi:ss'),1,5,'A','B',to_date('2019-01-01 11:00:00', 'yyyy/mm/dd hh24:mi:ss'), Equipage(Avionneur('Jean','Pilote')), Qualite(indiceQualite('Qualité', 5, 1), indiceQualite('Ponctualité', 5, 1), indiceQualite('Sécurité', 5, 1)));
-insert into VOL values (2,3,to_date('2019-01-01 11:10:00', 'yyyy/mm/dd hh24:mi:ss'),5,6,'B','A',to_date('2019-01-01 12:00:00', 'yyyy/mm/dd hh24:mi:ss'), Equipage(Avionneur('Marc','Pilote')), Qualite(indiceQualite('Qualité', 5, 1), indiceQualite('Ponctualité', 5, 1), indiceQualite('Sécurité', 5, 1)));
-insert into VOL values (3,3,to_date('2019-01-01 10:00:00', 'yyyy/mm/dd hh24:mi:ss'),5,6,'A','B',to_date('2019-01-01 11:00:00', 'yyyy/mm/dd hh24:mi:ss'), Equipage(Avionneur('Robert','Pilote')), Qualite(indiceQualite('Qualité', 5, 1), indiceQualite('Ponctualité', 5, 1), indiceQualite('Sécurité', 5, 1)));
-insert into VOL values (4,2,to_date('2019-01-01 13:00:00', 'yyyy/mm/dd hh24:mi:ss'),3,1,'B','A',to_date('2019-01-01 14:00:00', 'yyyy/mm/dd hh24:mi:ss'), Equipage(Avionneur('Daniel','Pilote')), Qualite(indiceQualite('Qualité', 5, 1), indiceQualite('Ponctualité', 5, 1), indiceQualite('Sécurité', 5, 1)));
-insert into VOL values (5,1,to_date('2019-01-01 14:00:00', 'yyyy/mm/dd hh24:mi:ss'),1,4,'A','B',to_date('2019-01-01 15:00:00', 'yyyy/mm/dd hh24:mi:ss'), Equipage(Avionneur('Khaled','Pilote')), Qualite(indiceQualite('Qualité', 5, 1), indiceQualite('Ponctualité', 5, 1), indiceQualite('Sécurité', 5, 1)));
-insert into VOL values (6,2,to_date('2019-01-01 15:00:00', 'yyyy/mm/dd hh24:mi:ss'),4,1,'B','A',to_date('2019-01-01 16:00:00', 'yyyy/mm/dd hh24:mi:ss'), Equipage(Avionneur('Bashar','Pilote')), Qualite(indiceQualite('Qualité', 5, 1), indiceQualite('Ponctualité', 5, 1), indiceQualite('Sécurité', 5, 1)));
-insert into VOL values (8,3,to_date('2019-01-01 11:00:00', 'yyyy/mm/dd hh24:mi:ss'),6,5,'B','A',to_date('2019-01-01 12:00:00', 'yyyy/mm/dd hh24:mi:ss'), Equipage(Avionneur('Gontran','Pilote')), Qualite(indiceQualite('Qualité', 5, 1), indiceQualite('Ponctualité', 5, 1), indiceQualite('Sécurité', 5, 1)));
+insert into VOL values (1,1,to_date('2019-01-01 10:00:00', 'yyyy/mm/dd hh24:mi:ss'),1,5,'A','B',to_date('2019-01-01 11:00:00', 'yyyy/mm/dd hh24:mi:ss'), REF(EQUIPAGE('Paul','Pilote')), indice_qualite(1,2,3));
+insert into VOL values (2,3,to_date('2019-01-01 11:10:00', 'yyyy/mm/dd hh24:mi:ss'),5,6,'B','A',to_date('2019-01-01 12:00:00', 'yyyy/mm/dd hh24:mi:ss'), 'Paul', indice_qualite(1,2,3));
+insert into VOL values (3,3,to_date('2019-01-01 10:00:00', 'yyyy/mm/dd hh24:mi:ss'),5,6,'A','B',to_date('2019-01-01 11:00:00', 'yyyy/mm/dd hh24:mi:ss'), 'Jacques', indice_qualite(1,2,3));
+insert into VOL values (4,2,to_date('2019-01-01 13:00:00', 'yyyy/mm/dd hh24:mi:ss'),3,1,'B','A',to_date('2019-01-01 14:00:00', 'yyyy/mm/dd hh24:mi:ss'), 'Pierre', indice_qualite(1,2,3));
+insert into VOL values (5,1,to_date('2019-01-01 14:00:00', 'yyyy/mm/dd hh24:mi:ss'),1,4,'A','B',to_date('2019-01-01 15:00:00', 'yyyy/mm/dd hh24:mi:ss'), 'Paul', indice_qualite(1,2,3));
+insert into VOL values (6,2,to_date('2019-01-01 15:00:00', 'yyyy/mm/dd hh24:mi:ss'),4,1,'B','A',to_date('2019-01-01 16:00:00', 'yyyy/mm/dd hh24:mi:ss'), 'Jacques', indice_qualite(1,2,3));
+insert into VOL values (8,3,to_date('2019-01-01 11:00:00', 'yyyy/mm/dd hh24:mi:ss'),6,5,'B','A',to_date('2019-01-01 12:00:00', 'yyyy/mm/dd hh24:mi:ss'), 'Pierre', indice_qualite(1,2,3));
